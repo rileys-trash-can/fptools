e367294942f6656206eb6d73a86699661de39c2b
Riley L. <riley@e926.de>
2024-04-21 16:57:43 +0200

    update smallorange

--- cmd/fputils/export.smallorange.sh
+++ cmd/fputils/export.smallorange.sh
@@ -4,13 +4,14 @@ convert ~/Pictures/Vec2/export.png \
 	-background black \
 	-channel RGB \
 	-negate \
-	-resize 794x397 \
-	-ordered-dither o4x4 \
+	-resize 792x396 \
 	-fill black \
 	-gravity west \
+	-depth 1 \
 	-splice 30x0 \
 	$@
 
 # dither above fill
+#	-ordered-dither o4x4 \
 #bellow fill
 # 	+opaque white \
